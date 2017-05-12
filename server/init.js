e55c20349cbe069ad662242a387a221b33b4559a
Ronan Le Pivaingt <r.lepivaingt@gmail.com>
2017-05-12 17:24:12 +0200

    Update : database structure is OK for the stopovers table

--- server/init.js
+++ server/init.js
@@ -1,14 +1,43 @@
+// Initializing DB with sqlite3 before populating it with Knex
 var sqlite3 = require('sqlite3');
-//var db = new sqlite3.Database('admin/db.sqlite');
-//db.serialize(function() {
-      //db.run("CREATE TABLE IF NOT EXISTS stopover (id INTEGER PRIMARY KEY AUTOINCREMENT" +
-              //", title STRING, description TEXT)");
-//});
-//db.close();
+var db = new sqlite3.Database('server/db.sqlite');
 
+db.serialize(function() {
+      db.run(
+        "CREATE TABLE IF NOT EXISTS stopovers " +
+        "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        "title STRING, " +
+        "description TEXT, " +
+        "sorting INTEGER, " +
+        "polyline TEXT, " +
+        "startLat FLOAT, " +
+        "startLng FLOAT, " +
+        "endLat FLOAT, " +
+        "endLng FLOAT " +
+        ")");
+});
+db.close();
+
+/* Knex create table example
+knex.schema.createTable('users', function (table) {
+  table.increments();
+  table.string('name');
+  table.timestamps();
+})
+*/
+
+/*
 var knex = require('knex')({
     client: 'sqlite3',
     connection: {
-      filename: "admin/db.sqlite"
+      filename: "server/db.sqlite"
     }
 });
+knex.schema.createTableIfNotExists('stopovers', function (table) {
+  table.increments();
+  table.string('title');
+  table.string('description');
+  table.integer('sorting');
+  table.timestamps();
+});
+*/
